a8cd3b51f539cdc8bfc76838e676ca2e67bb5377
Redwan Diriye <ridwanmohamedco@outlook.com>
2021-10-07 08:37:17 -0500

    first commit

---
+++ sub_join.sql
@@ -0,0 +1,19 @@
+-- Question 1
+SELECT *
+from address , customer
+WHERE district = 'Texas'
+JOIN INNER state_base
+ON 
+
+
+
+
+
+-- Question 2
+SELECT first_name , last_name , amount
+FROM payment, customer
+WHERE amount > 6.99
+-- Answer: here
+
+
+
